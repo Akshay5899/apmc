971b44a4fa808fd1de444ec376692596de5bafd4
Akshay5899 <84435118+Akshay5899@users.noreply.github.com>
2025-11-23 19:53:14 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '/apmc',        // <--- add this
-  assetPrefix: '/apmc',     // <--- add this
+  output: "export",                // for static export
+  trailingSlash: true,             // ensures proper paths
+  basePath: isProd ? "/apmc" : ""  // GitHub Pages repo name
 };
 
 module.exports = nextConfig;
