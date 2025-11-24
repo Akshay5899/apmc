ddcaa6afa5f765224803b36fb5827b82ceb0fcf2
sagar jadhav <sagar.jadhav@brawizz.com>
2025-11-24 22:19:57 +0530

    Updated config and package.json

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig = {
-  reactStrictMode: true,
-  output: "export",                // for static export
-  trailingSlash: true,             // ensures proper paths
-  basePath: isProd ? "/apmc" : ""  // GitHub Pages repo name
+module.exports = {
+  output: "export",
+  basePath: isProd ? "/apmc" : "",
+  trailingSlash: true
 };
-
-module.exports = nextConfig;
