1f8ad231427bfef627a9b188399ab35c84c9ccff
Akshay5899 <84435118+Akshay5899@users.noreply.github.com>
2025-11-24 21:30:47 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  reactStrictMode: true,
   output: "export",
-  trailingSlash: true,
   basePath: isProd ? "/apmc" : "",
-  assetPrefix: isProd ? "/apmc/" : ""
+  trailingSlash: true
 };
 
 module.exports = nextConfig;
